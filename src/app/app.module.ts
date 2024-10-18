2a4db0bff530ea18567debb6ba2082261ba73bfe
mfaisalmjanjua <m.faisal.m.janjua@gmail.com>
2024-10-18 16:00:06 +0500

    update datasheet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,12 @@ import { TblComponent } from './tbl/tbl.component';
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000',
     }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
